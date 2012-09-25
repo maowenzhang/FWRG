40de298bf7e877ca708b3f2db8e6505e03ad24fa
Maowen <maowen.zhang@hotmail.com>
2012-09-25 11:57:47 +0800

    remove duplicated files, update how to use git document

--- src/index.js
+++
@@ -1,6 +0,0 @@
-var io = require('socket.io'); 
-var socket = io.connect('http://localhost');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
