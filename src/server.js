1e4889077f62c9e8e61cb0ee50aacd68da76f7f1
Maowen <maowen.zhang@hotmail.com>
2012-10-29 10:04:18 +0800

    add changes to database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var ecstatic = require('ecstatic')(__dirname + '/public');
-var server = http.createServer(ecstatic);
 debugger
 var server = http.createServer(ecstatic)
   , io = require('socket.io').listen(server)
@@ -28,38 +27,5 @@ io.sockets.on('connection', function (socket) {
   });
 });
 
-
-
-var Db = require('mongodb').Db;
-var Server = require('mongodb').Server;
-var client = new Db('test', new Server('127.0.0.1', 27017, {}), {safe:false}),
-	test= function (err, collection) {
-      collection.insert({a:2}, function(err, docs) {
-
-        collection.count(function(err, count) {
-          //test.assertEquals(1, count);
-		  console.log("lori***: count: " + count);
-        });
-
-        // Locate all the entries using find
-        collection.find().toArray(function(err, results) {
-          //test.assertEquals(1, results.length);
-          //test.assertTrue(results[0].a === 2);
-		  console.log("lori***: a: " + results[0].a);
-		  
-		  for(var i=0; i<results.length; i++) {
-			console.log(results[0].a);			
-		  }
-		  
-          // Let's close the db
-          client.close();
-        });
-      });
-    };
-
-client.open(function(err, p_client) {
-  client.collection('test_insert', test);
-});
-
-
-
+// data storage
+//var datamgr = require('./datamgr.js');
