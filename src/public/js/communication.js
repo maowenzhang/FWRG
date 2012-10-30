ec949f6e9e4ca7bf845e28b3ebd12a45f0926e66
Maowen <maowen.zhang@hotmail.com>
2012-10-30 17:05:00 +0800

    changes to communication.js

--- src/public/js/communication.js
+++ src/public/js/communication.js
@@ -1,7 +1,29 @@
-//var io = require('socket.io');
 var socket = io.connect('http://localhost:8080');
-console.log("socket connected at localhost:8080");
 
+// Connected to server
+socket.on('connect', function () {
+    console.log("socket connected at localhost:8080");
+	sendUserLogin("lori");
+});
+
+// Disconnected to server
+socket.on('disconnect', function () {
+	console.log("socket disconnected");
+	sendUserEnd("lori");
+});
+
+// On getting update from server
+socket.on('update', function(type, data) {
+	console.log("update from server");
+	// TODO: add handling when get updates from server
+	// Need to handle for different updating events and take proper action (such as: redraw game table)
+	// data: gameState object
+	//
+	
+}
+
+// Util method for client to call/sending message to server
+//
 function sendUpdateToServer(type, data) {
     socket.emit("update", type, data);
 }
