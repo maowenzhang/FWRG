8fc1dec8655bc0c8dc4d79e379b0ec2a41e63805
Andre Zhang <Weihu.Zhang@autodesk.com>
2012-11-07 22:12:18 +0800

    game view update..
    draw players according position, session, left and right side.
    clean some unused code.

--- src/public/js/gameview.js
+++ src/public/js/gameview.js
@@ -18,14 +18,15 @@ ImageLoader.prototype = {
 		self.loadtype = loadtype;
 		if(self.loadtype == "image"){
 			self.content = new Image();
-			self.content.onload = function(){
+			self.content.src = src; 
+			// onload event seems not be called automatically, call it directly
+			//self.content.onload = function(){
 				console.log('image loaded');
 				if(self.oncomplete){
 					self.event.currentTarget = self.content;
 					self.oncomplete(self.event);
 				}
-			};
-			self.content.src = src; 
+			//};			
 		}
 	}
 };
@@ -122,17 +123,25 @@ function GameView(paper) {
 	this.center = view.center;
 	this.margin = 10; //10px
 	this.offset = 16;
+	this.cardSize = { width:0, height:0};
 
+	// should be input, for test
+	this.gameState = {
+	players: [ {name:"Lori", pos: 1, leftPlayer: "Andre", rightPlayer: "Bruce", avatar: "avatar", cards: []}
+				, {name:"Bruce", pos: 2, leftPlayer: "Lori", rightPlayer: "Andre", avatar: "avatar", cards: []}
+				, {name:"Andre", pos: 3, leftPlayer: "Bruce", rightPlayer: "Lori", avatar: "avatar", cards: []} ],
+	activePlayer : {name:"Lori", pos: 1, leftPlayer: "Andre", rightPlayer: "Bruce", avatar: "avatar", cards: []}
+	};
 	// track the active player, which needs to deliver cards or
 	// pass.
 	this.activePlayer = null;
 	
 	// store the hit test result.
 	this.hitObject = null;
-	var cardObjects = []; // record raster object of current player
-
-	this.sessionPlayer = null;
-	this.gameState = null;
+	
+	this.sessionPlayer = {name:"Lori", pos: 1, leftPlayer: "Andre", rightPlayer: "Bruce", avatar: "avatar", cards: []};
+	this.players = null;//gameSession.joinedPlayers;
+	
 	this.playerViews = [];
 	var self = this;
 
@@ -150,7 +159,7 @@ function GameView(paper) {
 	}
 
 	function drawLordFlag(avatarPos, refVec) {
-		var lordImg = resImages['Lord'];
+		var lordImg = self.resImages['Lord'];
 		refVec = refVec.normalize(40+lordImg.height);
 		var lord = new paper.Raster(lordImg);
 		lord.position = new Point(avatarPos.x+refVec.x, avatarPos.y+refVec.y);
@@ -158,11 +167,50 @@ function GameView(paper) {
 	}
 
 	this.update = function(data) {
-		this.gameState = data.gameState;
-		this.sessionPlayer = data.sessionPlayer;
+		//this.gameState = data.gameState;
+		//this.sessionPlayer = data.sessionPlayer;
+		
+		for(var i = 0; i < this.gameState.players.length; ++i)
+		{
+			if(this.gameState.players[i].name == data.sessionPlayer.name)
+			{
+				this.sessionPlayer = this.gameState.players[i];
+				break;
+			}
+		}
 		
 		cleanPlayers();
 		drawPlayers();
+	}
+	
+	this.drawForDeliverCard = function(data)
+	{
+		var receiveCardPlayer = data.receiveCardPlayer;		
+		// redraw cards for this player		
+		updatePlayerView(receiveCardPlayer);
+	}
+	
+	function updatePlayerView(player)
+	{
+		for(var j = 0; j < self.playerViews.length; ++j)
+		{
+			if(player && player.name == self.playerViews[j].playerName)
+			{
+				for(var cardObj in self.playerViews[j].cardObjects)
+					cardObj.remove();
+				self.playerViews[j].splice(0, self.playerViews[j].cardObjects.length);
+				var isSessionPlayer = (self.sessionPlayer.name == player.name);
+				var atLeftSide = 0;
+				if(!isSessionPlayer)
+				{
+					if(self.sessionPlayer.leftPlayer.name == player.name)
+						atLeftSide = 1;
+				}
+				drawCards(player, self.playerViews[j], isSessionPlayer, atLeftSide);
+				view.draw();
+				break;
+			}
+		}
 	}
 	
 	function cleanPlayers() {
@@ -203,38 +251,41 @@ function GameView(paper) {
 		var players = self.gameState.players;
 		if(!players || !sessionPlayer)
 			return;
-		
-		var card = null;
-		var x, y;
-		var suit, rank;
-		var id;
-		var avatarImg, avatarPos, refVec, playerName;
-		var numOtherPlayers = 0;
-		var existing;
-		for(var i = 0; i < players.length; ++i) {
-			var player = players[i];
-			var isSessionPlayer = (player.name == sessionPlayer.name);
 			
-			// check if the player has been drawn.
-			existing = false;
-			for(var j = 0; j < self.playerViews.length; ++j) {
-				if(player && player.name == self.playerViews[j].playerName) {
-					existing = true;
-					break;
+		drawPlayerView(sessionPlayer, true, 0);
+		for(var i = 0; i < players.length; ++i)
+		{
+			if(players[i].name == sessionPlayer.leftPlayer)
+			{
+				// draw left side player
+				drawPlayerView(players[i], false, 1);
+			}
+			else if(players[i].name == sessionPlayer.rightPlayer)
+			{
+				// draw right side player
+				drawPlayerView(players[i], false, 0);
+			}
+		}
+		
+		function drawPlayerView(player, isSessionPlayer, atLeft)
+		{
+			if(!player)
+				return;
+			for(var j = 0; j < self.playerViews.length; ++j)
+			{
+				if(player && player.name == self.playerViews[j].playerName)
+				{
+					// if the player profile is drawn
+					drawCards(sessionPlayer, self.playerViews[j], isSessionPlayer, atLeft);
+					return;
 				}
 			}
+
+			var x, y;
+			var id;
+			var avatarImg, avatarPos, refVec, playerName;
 			
-			drawCards(player, isSessionPlayer, numOtherPlayers);
-			
-			if(existing) {
-				if(player.name != sessionPlayer.name)
-					numOtherPlayers++;
-			}
-			
-			if(existing) continue;
-			
-			//var cards = player.cards;
-			// draw the avatar
+			var length = resImages.length;
 			avatarImg = resImages[player.avatar];
 			avatar = new paper.Raster(avatarImg);
 			
@@ -245,9 +296,9 @@ function GameView(paper) {
 				avatarPos = new Point(x, y);
 				refVec = new Point(1, -1);
 			} else {
-				var offset = self.offset+self.cardW*2.5;
+				var offset = self.offset+self.cardSize.width*2.5;
 				// calc the avatar pos for this player
-				if(numOtherPlayers == 0) {
+				if(atLeft) {
 					x = offset/2;
 					y = self.viewSize.height/2;
 					avatarPos = new Point(x, y);
@@ -258,7 +309,6 @@ function GameView(paper) {
 					avatarPos = new Point(x, y);
 					refVec = new Point(1, -1);
 				}
-				numOtherPlayers++;
 			}
 			
 			// set the position of the avatar
@@ -278,22 +328,23 @@ function GameView(paper) {
 			if(player.isLord) {
 				drawLordFlag(avatarPos, refVec);
 			}
+			var playerView = { playerName: player.name, nameObject: playerName, avatarObject: avatar};
+			self.playerViews.push(playerView);
 			
-			self.playerViews[self.playerViews.length] = { 
-				playerName: player.name, nameObject: playerName, avatarObject: avatar
-			};
+			drawCards(sessionPlayer, playerView, isSessionPlayer, atLeft);
 		}
+		
 		view.draw();
 	}
 	
-	function drawCards(player, isSessionPlayer, numOtherPlayers) {
+	function drawCards(player, playerView, isSessionPlayer, atLeft) {
 		if(!player)
 			return;
 			
 		var cards = player.cards;
 		var x, y, card;
 		var num = cards.length - 1;
-		
+		playerView.cardObjects = [];
 		for(var i = 0; i < cards.length; ++i) {
 			card = cards[i];
 			if(isSessionPlayer)
@@ -305,31 +356,33 @@ function GameView(paper) {
 				cardRaster.name = card.id;
 				cardRaster.position = new Point(x+num*self.offset, y);
 				
-				cardObjects.push(cardRaster);
+				playerView.cardObjects.push(cardRaster);
 			}
 			else
 			{
 				var totalH = (cards.length-1)*self.offset + self.cardH;
-				var offset = self.offset+self.cardW*2.5;
-				x = (numOtherPlayers==0) ? offset : (self.viewSize.width-offset);
+				var offset = self.offset+self.cardSize.width*2.5;
+				x = atLeft ? offset : (self.viewSize.width-offset);
 				y = (self.viewSize.height - totalH)/2;				
 				var cardRaster = new paper.Raster(resImages['rear']);
 				cardRaster.name = 'rear'+i;
 				cardRaster.position = new Point(x, y+num*self.offset);
+				
+				playerView.cardObjects.push(cardRaster);
 			}
 		}
 	}
-
+	
 	var currentPlayer = 0;
 	var timerId;
 
 	this._calcMyPositionInfo = function(numCards) {
-		var totalW = (numCards-1)*this.offset + this.cardW;
-		var offset = this.margin + this.cardH/2;
+		var totalW = (numCards-1)*this.offset + this.cardSize.width;
+		var offset = this.margin + this.cardSize.height/2;
 		return {
 			totoalW: totalW,
 			yOffset: offset,
-			center: { x: (this.viewSize.width - totalW)/2 + this.cardW/2 + 0.5, y: this.viewSize.height - offset }
+			center: { x: (this.viewSize.width - totalW)/2 + this.cardSize.width/2 + 0.5, y: this.viewSize.height - offset }
 		};
 	}
 
@@ -338,9 +391,8 @@ function GameView(paper) {
 	{
 		// get the dimension of the card
 		var cardImg = resImages['1-3'];
-		console.log(cardImg);
-		this.cardW = cardImg.width;
-		this.cardH = cardImg.height;
+		this.cardSize.width = cardImg.width;
+		this.cardSize.height = cardImg.height;
 		
 		// some other useful location info
 		this.myPositionInfo = this._calcMyPositionInfo(20);
@@ -449,6 +501,7 @@ function GameView(paper) {
 		}
 	}
 	
+	// to be removed
 	function deliverCards()
 	{
 		if(deck.cards.length == 0) {			
@@ -473,11 +526,12 @@ function GameView(paper) {
 				}
 			}			
 			
-			var yOffset = self.margin + self.cardH/2;
-			var x = (self.viewSize.width - self.cardW*3 - self.margin*1.5)/2 + self.cardW/2;
+			var yOffset = self.margin + self.cardSize.height/2;
+			var x = (self.viewSize.width - self.cardSize.width*3 - self.margin*1.5)/2 + self.cardSize.width/2;
 			var y = yOffset;
 		
 			// The last 3 cards should be visible for all players.
+			var rearImg = resImages['rear'];
 			var card1 = deck.cards[deck.cards.length-1];
 			id = getCardImgId(card1.suit.value, card1.rank.value);
 			card = new paper.Raster(document.getElementById(id));
@@ -486,12 +540,12 @@ function GameView(paper) {
 			var card2 = deck.cards[deck.cards.length-2];
 			id = getCardImgId(card2.suit.value, card2.rank.value);
 			card = new paper.Raster(document.getElementById(id));
-			card.position = new Point(x+self.cardW+self.margin/2, y);
+			card.position = new Point(x+self.cardSize.width+self.margin/2, y);
 			
 			var card3 = deck.cards[deck.cards.length-3];
 			id = getCardImgId(card3.suit.value, card3.rank.value);
 			card = new paper.Raster(document.getElementById(id));
-			card.position = new Point(x+2*(self.cardW+self.margin/2), y);
+			card.position = new Point(x+2*(self.cardSize.width+self.margin/2), y);
 			
 			deck.cards.remove(deck.cards[0]);
 			deck.cards.remove(deck.cards[0]);
@@ -537,8 +591,8 @@ function GameView(paper) {
 			
 			// calculate the position of the out cards, it must be in the center of the view
 			var center = view.center;
-			var totalLength = self.cardW + self.offset * selectedCards.length;
-			var startW = center.x - totalLength / 2 + self.cardW / 2;
+			var totalLength = self.cardSize.width + self.offset * selectedCards.length;
+			var startW = center.x - totalLength / 2 + self.cardSize.width / 2;
 			var startH = center.y;
 			
 			for(var i = 0; i < selectedCards.length; ++i)
@@ -553,89 +607,28 @@ function GameView(paper) {
 				player.cards.remove(card);
 			}
 			
-			updateCardsPosition(player.cards);
+			updatePlayerView(player);
 			
 			view.draw();
 		}
 	}
-	
-	function updateCardsPosition(cards)
-	{
-		// just redraw the cards for this player
-		while(cardObjects.length > 0)
-		{
-			var card = cardObjects[0];
-			cardObjects.remove(card);
-			card.remove();
-		}
-		
-		var x = self.myPositionInfo.center.x; 
-		var y = self.myPositionInfo.center.y;
-		for(var j = 0; j < cards.length; ++j)
-		{
-			suit = cards[j].suit.value;
-			rank = cards[j].rank.value;
-			id = getCardImgId(suit, rank);
-			//console.log(id);
-			var cardRaster = new paper.Raster(document.getElementById(id));
-			cardRaster.name = suit + '-' + rank;
-			cardRaster.position = new Point(x+j*self.offset, y);
-			cardObjects.push(cardRaster);
-		}
-	}
-	
-	function drawCard(card, currentPlayer)
-	{
-		var cards = self.players[currentPlayer].cards;
-		var x, y;
-		
-		if(currentPlayer == 0)
-		{
-			x = self.myPositionInfo.center.x; 
-			y = self.myPositionInfo.center.y;
-			// always draw the last delived card
-			var j = cards.length - 1;
-			var suit = card.suit.value;
-			var rank = card.rank.value;
-			var id = getCardImgId(suit, rank);
-			//console.log(id);
-			var cardRaster = new paper.Raster(document.getElementById(id));
-			cardRaster.name = suit + '-' + rank;
-			cardRaster.position = new Point(x+j*self.offset, y);
-			
-			cardObjects.push(cardRaster);
-		}
-		else
-		{
-			var totalH = (cards.length-1)*self.offset + self.cardH;
-			var offset = self.offset+self.cardW*2.5;
-			x = (currentPlayer == 2) ? offset : (self.viewSize.width-offset);
-			y = (self.viewSize.height - totalH)/2;				
-			var j = cards.length - 1;
-			var cardRaster = new paper.Raster(document.getElementById('rear'));
-			cardRaster.name = 'rear'+j;
-			cardRaster.position = new Point(x, y+j*self.offset);
-		}
-	}
-	
+
 	function drawButtons() {
 		var buttons = ['chupai', 'buchu', 'tishi'];
-		var btnImg = document.getElementById(buttons[0]);
-		var imgForSize = new Image();
-		imgForSize.src = btnImg.src;
-		var totalW = imgForSize.width*3 + 2*self.offset;
-		var x = (self.viewSize.width - totalW)/2 + imgForSize.width/2;
-		var y = self.viewSize.height - self.cardH - 3*self.offset;
+		var btnImg = resImages[buttons[0]];
+		var totalW = btnImg.width*3 + 2*self.offset;
+		var x = (self.viewSize.width - totalW)/2 + btnImg.width/2;
+		var y = self.viewSize.height - self.cardSize.height - 3*self.offset;
 		
 		var btn;
 		for(var i = 0; i < buttons.length; ++i)
 		{
-			var x1 = x + i * imgForSize.width + i * self.offset;
-			btn = new paper.Raster(document.getElementById(buttons[i]));
+			var x1 = x + i * btnImg.width + i * self.offset;
+			var image = resImages[buttons[i]];
+			btn = new paper.Raster(image);
 			btn.position = new Point(x1, y);
 			btn.name = buttons[i];
 		}
-		imgForSize = null;
 		view.draw();
 	}
 }
